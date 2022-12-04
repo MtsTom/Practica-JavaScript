88031e604762f8a22e9c743aef7935b3b3b867ee
Matías Tomasini <tomasinimatiasinc@gmail.com>
2022-12-04 15:45:18 -0300

    scroll button

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from './dom/menu_hamburguesa.js';
 import { digitalClock, alarm } from './dom/reloj.js';
 import { moveBall, shortcuts } from './dom/teclado.js';
 import countdown from './dom/cuenta_regresiva.js';
+import scrollTopButton from './dom/button_scroll.js';
 
 const d = document;
 
@@ -10,6 +11,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
   digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
   alarm('assets/alarma.mp3', '#activar-alarma', '#desactivar-alarma');
   countdown('countdown', 'Nov 25, 2023 03:23:19', 'Feliz cumpleaños 🤓');
+  scrollTopButton;
 });
 
 d.addEventListener('keydown', (e) => {
