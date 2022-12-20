a3a7f51b7f08a3b979e96879f340d58808ae776e
Matías Tomasini <tomasinimatiasinc@gmail.com>
2022-12-20 18:50:06 -0300

    API Fetch

--- js/dom/ajax.js
+++ js/dom/ajax.js
@@ -7,7 +7,7 @@
     if (xhr.readyState !== 4) return;
     console.log(xhr);
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log('éxito');
+      //console.log('éxito');
       //console.log(xhr.responseText);
       let json = JSON.parse(xhr.responseText);
       console.log(json);
@@ -20,13 +20,46 @@
 
       $xhr.appendChild($fragment);
     } else {
-      console.log('error');
+      //console.log('error');
       let message = xhr.statusText || 'Ocurrió un error';
-      $xhr.innerHTML = `Erro ${xhr.status}: ${message}`;
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
   });
 
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  //xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.open('GET', 'assets/users.json');
 
   xhr.send();
+})();
+
+(() => {
+  const $fetch = document.getElementById('fetch'),
+    $fragment = document.createDocumentFragment();
+
+  //fetch("assets/users.json")
+  fetch('https://jsonplaceholder.typicode.com/users')
+    /* .then((res) => {
+      console.log(res);
+      return res.ok ? res.json() : Promise.reject(res);
+    }) */
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      //console.log(json);
+      //$fetch.innerHTML = json;
+      json.forEach((el) => {
+        const $li = document.createElement('li');
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      //console.log(err);
+      let message = err.statusText || 'Ocurrió un error';
+      $fetch.innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      //console.log("Esto se ejecutará independientemente del resultado de la Promesa Fetch")
+    });
 })();
