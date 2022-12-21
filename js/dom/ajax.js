a589cc5a3db2fa5d876dbe46cd291c4aa3f65889
Matías Tomasini <tomasinimatiasinc@gmail.com>
2022-12-21 10:08:54 -0300

    API Fetch + Async-Await

--- js/dom/ajax.js
+++ js/dom/ajax.js
@@ -10,7 +10,7 @@
       //console.log('éxito');
       //console.log(xhr.responseText);
       let json = JSON.parse(xhr.responseText);
-      console.log(json);
+      //console.log(json);
 
       json.forEach((el) => {
         const $li = document.createElement('li');
@@ -62,4 +62,38 @@
     .finally(() => {
       //console.log("Esto se ejecutará independientemente del resultado de la Promesa Fetch")
     });
+})();
+
+(() => {
+  const $fetchAsync = document.getElementById('fetch-async'),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch('https://jsonplaceholder.typicode.com/users'),
+        json = await res.json();
+
+      console.log(res, json);
+
+      if (!res.ok) {
+        throw { status: res.status, statusText: res.statusText };
+      }
+
+      json.forEach((el) => {
+        const $li = document.createElement('li');
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err);
+      let message = err.statusText || 'Ocurrió un error';
+      $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+    } finally {
+      console.log('Esto se ejecutará independientemente del try catch');
+    }
+  }
+
+  getData();
 })();
