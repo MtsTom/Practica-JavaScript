cb8266177c47dda14200a33111da34ed959576b1
Matías Tomasini <tomasinimatiasinc@gmail.com>
2022-12-22 11:12:15 -0300

    Axios + Async-Await

--- js/dom/ajax.js
+++ js/dom/ajax.js
@@ -105,7 +105,7 @@
   axios
     .get('https://jsonplaceholder.typicode.com/users')
     .then((res) => {
-      console.log(res);
+      //console.log(res);
       let json = res.data;
       json.forEach((el) => {
         const $li = document.createElement('li');
@@ -115,11 +115,41 @@
       $axios.appendChild($fragment);
     })
     .catch((err) => {
-      console.log(err.response);
+      //console.log(err.response);
       let message = err.response.statusText || 'Ocurrió un error';
       $axios.innerHTML = `Error ${err.response.status}: ${message}`;
     })
     .finally(() => {
-      console.log('Esto se ejecutará independientemente del resultado Axios.');
+      //console.log('Esto se ejecutará independientemente del resultado Axios.');
     });
+})();
+
+(() => {
+  const $axiosAsync = document.getElementById('axios-async'),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await axios.get('https://jsonplaceholder.typicode.com/users'),
+        json = await res.data;
+
+      //console.log(res, json);
+
+      json.forEach((el) => {
+        const $li = document.createElement('li');
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      //console.log(err.response);
+      let message = err.response.statusText || 'Ocurrió un error';
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      //console.log("Esto se ejecutará independientemente del try... catch");
+    }
+  }
+
+  getData();
 })();
